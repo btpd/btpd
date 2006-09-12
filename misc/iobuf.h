c9ea09b1ad72ccb342802622943c98f0fa465799
Richard Nyberg <rnyberg@murmeldjur.se>
2006-09-12 08:52:28 +0000

    Enable printf format type checking for buf_print.

--- misc/iobuf.h
+++ misc/iobuf.h
@@ -11,6 +11,7 @@ struct io_buffer {
 int buf_init(struct io_buffer *iob, size_t size);
 int buf_grow(struct io_buffer *iob, size_t size);
 int buf_write(struct io_buffer *iob, const void *data, size_t size);
+__attribute__((format (printf, 2, 3)))
 int buf_print(struct io_buffer *iob, const char *fmt, ...);
 
 #define buf_swrite(iob, s) buf_write(iob, s, sizeof(s) - 1)
