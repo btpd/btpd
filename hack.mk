eb22256aa5165078b8cf889131d86655e2f34683
Richard Nyberg <rnyberg@murmeldjur.se>
2009-01-15 12:21:58 +0100

    git clean is a better way to clean the tree.

--- hack.mk
+++
@@ -1,4 +0,0 @@
-clean:
-	find . -name \*~ -print0 | xargs -0 rm
-	find . -name \*.in -print0 | xargs -0 rm
-	rm -rf aclocal.m4 autom4te.cache compile config.* configure depcomp install-sh missing btpd-*.tar.gz
